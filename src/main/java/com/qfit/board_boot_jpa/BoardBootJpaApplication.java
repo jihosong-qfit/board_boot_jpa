cdfbece40a25e3400d16f1d2ef2036ee1dfe179c
jihosong-qfit <imprisonment2020@gmail.com>
2024-06-26 10:02:23 +0900

    24.06.26 first commit

---
+++ src/main/java/com/qfit/board_boot_jpa/BoardBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.qfit.board_boot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qfit/board_boot_jpa/BoardBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qfit.board_boot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
